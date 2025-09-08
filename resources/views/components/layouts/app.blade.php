e66dcf45cbbdde80b83dd8a223e870861f6bc7d1
Nessalyn <149561544+NessLM@users.noreply.github.com>
2025-09-08 10:12:13 +0700

    Add Bootstrap Icons stylesheet to app layout and remove from sidebar component

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,6 +14,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
 
   {{-- Komponen lain boleh nge-push asset ke <head> --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('assets/css/components/sidebar.css') }}">
 
   {{-- (opsional) CSS global app-mu --}}
